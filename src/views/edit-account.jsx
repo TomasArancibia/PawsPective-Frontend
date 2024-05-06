4522b53d2aade8279f9dbfe5f2e07db29ca260f7
tonyroxtar <antonio.chamal@gmail.com>
2024-05-06 19:16:37 -0400

    funciona FontAwesome

---
+++ src/views/edit-account.jsx
@@ -0,0 +1,11 @@
+import Navbars from "../components/navbars";
+
+const EditAccount = () => {
+    return (
+        <div>
+            <Navbars />
+        </div>
+    );
+};
+
+export default EditAccount;
