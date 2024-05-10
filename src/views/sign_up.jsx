09a2c28d86a33b1937002808fad24709e6512f2b
tomas arancibia domper <tomasarancibia@uc.cl>
2024-05-10 19:53:23 -0400

    SignUP view

--- src/views/sign_up.jsx
+++ src/views/sign_up.jsx
@@ -1,25 +1,33 @@
 import "./sign_up.css"
-
+import Image from "../assets/AnimalsPhoto.png"
 
 
 const SignUp = () => {
 
     return (
         <>
+            <div className="container-fluid d-flex justify-content-center">
+                <div className="circle-photo">
+                    <img src={Image} alt="Animals" className="circle-photo" />
+                </div>
+            </div>
             <div className="form-sign-up">
-                <div className="name-lastname-input">
-                    <input className="form-control me-3" type="name" placeholder="Name" />
-                    <input className="form-control ms-3" type="Lastname" placeholder="LastName" />
+                <div className="name-lastname-input container-fluid">
+                    <input className="form-control me-1" type="name" placeholder="Name" />
+                    <input className="form-control ms-1" type="Lastname" placeholder="LastName" />
                 </div>
 
-                <div className="username-age-input">
-                    <input className="form-control me-3" type="Username" placeholder="Username" />
-                    <input className="form-control ms-3 w-25" type="number" placeholder="1" defaultValue={1} />
+                <div className="username-age-input container-fluid">
+                    <input className="form-control me-1" type="Username" placeholder="Username" />
+                    <input className="form-control ms-1 w-25" type="number" placeholder="1" defaultValue={1} />
                 </div>
-                <div className="email-password">
-                    <input className="form-control me-3 full-input" type="email" placeholder="Email" />
-                    <input className="form-control me-3 full-input" type="password" placeholder="Password" />
+                <div className="email-password container-fluid">
+                    <input className="form-control me-1 full-input" type="email" placeholder="Email" />
+                    <input className="form-control me-1 full-input" type="password" placeholder="Password" />
                 </div>
+            </div>
+            <div className="container-fluid d-flex justify-content-center">
+                <button className="signUp-btn btn"> Sign Up </button>
             </div>
         </>
     );
