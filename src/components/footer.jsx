1b14789e1854a6b04570874712d085126c2e63bf
tomas arancibia domper <tomasarancibia@uc.cl>
2024-05-10 20:39:48 -0400

    Footer v1

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,16 +1,32 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faDog } from "@fortawesome/free-solid-svg-icons";
+import { faDog, faHouseChimney, faMagnifyingGlassLocation, faBasketShopping, faWheelchair, faUser} from "@fortawesome/free-solid-svg-icons";
+import { NavLink } from "react-router-dom";
+import "../App.css"
 
 
 
 const Footer = () => {
   return (
     <div>
-      <nav className="navbar fixed-bottom">
-        <div className="container-fluid text-light">
-          <FontAwesomeIcon icon={faDog} /> Pawspective{" "}
-          <FontAwesomeIcon icon={faDog} flip="horizontal" />
-        </div>
+      <nav className="navbar fixed-bottom d-inline-flex p-2 ">
+        <NavLink className="circle-element mb-0 text-light" to="/sign_up">
+          <FontAwesomeIcon icon={faMagnifyingGlassLocation} className="navIcon" />
+        </NavLink>
+        <NavLink className="circle-element mb-0 text-light" to="/sign_up">
+          <FontAwesomeIcon icon={faDog} className="navIcon" />
+        </NavLink>
+        <NavLink className="circle-element mb-0 text-light" to="/sign_up">
+          <FontAwesomeIcon icon={faHouseChimney} className="navIcon" />
+        </NavLink>
+        <NavLink className="circle-element mb-0 text-light" to="/sign_up">
+          <FontAwesomeIcon icon={faBasketShopping} className="navIcon" />
+        </NavLink>
+        <NavLink className="circle-element mb-0 text-light" to="/sign_up">
+          <FontAwesomeIcon icon={faWheelchair} className="navIcon" />
+        </NavLink>
+        <NavLink className="circle-element mb-0 text-light" to="/">
+          <FontAwesomeIcon icon={faUser} className="navIcon" />
+        </NavLink>
       </nav>
     </div>
   );
