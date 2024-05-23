38257ea02490cafab3db23909a807f011e3d9fdd
tomas arancibia domper <tomasarancibia@uc.cl>
2024-05-23 13:11:46 -0400

    Fetch Create Post

--- src/components/NewPostForm.jsx
+++ src/components/NewPostForm.jsx
@@ -1,85 +1,71 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
+import Location from "./Location";
 
 
 const NewPostForm = () => {
-    const { store, actions } = useContext(Context);
-  
-    const handleSubmit = async (e) => {
-     
-        e.preventDefault();
-        
-      if (store.post.id) {
-        actions.putFetchPost(store.post.id);
-      } else {
-       await actions.handleSubmit(e);
-      }
-    };
-  
-    return (
-      <>
-        <form className="p-3" onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <label htmlFor="description" className="form-label">
-              Description
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="description"
-              placeholder="Enter Description for the post"
-              name="description"
-              value={store.post.description}
-              onChange={actions.handleChange}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="Source_Url" className="form-label">
-              Photo
-            </label>
-            <input
-              type="file"
-              className="form-control"
-              id="Source_Url"
-              name="Source_Url"
-              // value={store.post.Source_Url}
-              // onChange={actions.handleChangeFile}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="phomeNumber" className="form-label">
-              Phone Number
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="phoneNumber"
-              placeholder="Enter phone number"
-              name="phone"
-              value={store.post.phone}
-              onChange={actions.handleChange}
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="address" className="form-label">
-              Address
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="faddress"
-              placeholder="Enter your address"
-              name="address"
-              value={store.post.address}
-              onChange={actions.handleChange}
-            />
-          </div>
-          <button type="submit" className="btn btn-primary">
-            {store.post.id ? "Update" : "Submit"}
-          </button>
-        </form>
-      </>
-    );
+  const { store, actions } = useContext(Context);
+
+  const [formData, setFormData] = useState({});
+  const [file, setFile] = useState(null)
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
   };
-  
-  export default NewPostForm;
+
+  var fileData = new FormData();
+
+  const handleCreate = (e) => {
+    e.preventDefault()
+    fileData.append("post_data", JSON.stringify(formData))
+    fileData.append('source_url', file);
+    actions.createPost(fileData);
+    setFormData({})
+    setFile(null)
+    {/* Esta wea del fileData no se llena */ }
+  };
+
+  return (
+    <>
+      <form className="p-3" onSubmit={(e) => {
+        console.log("Save button clicked");
+        handleCreate(e);
+      }}>
+        <div className="mb-3">
+          <label htmlFor="description" className="form-label">
+            Description
+          </label>
+          <textarea
+            type="text"
+            className="form-control"
+            id="description"
+            placeholder="Enter Description for the post"
+            name="description"
+            value={formData.description}
+            onChange={(e) => {
+              setFormData({ ...formData, description: e.target.value });
+            }}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="Source_Url" className="form-label">
+            Upload Photo
+          </label>
+          <input
+            type="file"
+            className="form-control"
+            id="Source_Url"
+            name="Source_Url"
+            onChange={handleFileChange}
+          />
+        </div>
+        {/* <Location/> */}
+        <button type="submit" className="btn btn-primary">
+          {store.post.id ? "Update" : "Submit"}
+        </button>
+      </form>
+    </>
+  );
+};
+
+export default NewPostForm;
